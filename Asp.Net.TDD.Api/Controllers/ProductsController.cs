77b3feae274e316809ee670f971b6bcea856f5b0
Mujib517 <Mujib517@gmail.com>
2018-03-25 22:13:44 +0530

    Get Post

--- Asp.Net.TDD.Api/Controllers/ProductsController.cs
+++ Asp.Net.TDD.Api/Controllers/ProductsController.cs
@@ -1,9 +1,7 @@
-﻿using Asp.Net.TDD.Api.ViewModels;
+﻿using Asp.Net.TDD.Api.Mappers;
+using Asp.Net.TDD.Api.ViewModels;
 using AspNetTDD.Services;
 using AspNetTDD.Services.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
@@ -12,12 +10,37 @@ namespace Asp.Net.TDD.Api.Controllers
 {
     public class ProductsController : ApiController
     {
+        IProductService productSvc;
+
+        public ProductsController()
+        {
+            productSvc = new ProductService();
+        }
+
         public HttpResponseMessage Get()
         {
-            IProductService productSvc = new ProductService();
             var products = productSvc.Get();
 
+            var response = Mappers.ProductMapper.MapToViewModel(products);
             return Request.CreateResponse(HttpStatusCode.OK, products);
+        }
+
+        [HttpGet]
+        [Route("api/products/{id}")]
+        public HttpResponseMessage Get(string id)
+        {
+            var product = productSvc.GetById(id);
+            return Request.CreateResponse(HttpStatusCode.OK, product);
+        }
+
+        [HttpPost]
+        public HttpResponseMessage Post(Product product)
+        {
+            var productModel = ProductMapper.MapToModel(product);
+            IProductService productSvc = new ProductService();
+            productSvc.Save(productModel);
+
+            return Request.CreateResponse(HttpStatusCode.Created);
         }
     }
 }
