2ddbbfa9daca27e86a43b25ad117013aafb2ba33
Mujib517 <Mujib517@gmail.com>
2018-03-26 12:42:50 +0530

    Structuremap registries

--- Asp.Net.TDD.Api/Registries/ApiRegistry.cs
+++ Asp.Net.TDD.Api/Registries/ApiRegistry.cs
@@ -6,11 +6,15 @@ using System.Web;
 
 namespace Asp.Net.TDD.Api.Registries
 {
-    public class ApiRegistry:Registry
+    public class ApiRegistry : Registry
     {
         public ApiRegistry()
         {
-            
+            Scan(s =>
+            {
+                s.TheCallingAssembly();
+                s.WithDefaultConventions();
+            });
         }
     }
 }
